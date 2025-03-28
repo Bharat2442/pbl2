086c3d8ed39f3a9174c73b2e43ee27928e357386
Danu <bharatdanu24@gmail.com>
2025-03-28 22:28:44 +0530

    Initial commit

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -32,6 +32,9 @@ SECRET_KEY = 'd)=^c7!0-oqjmqve%(bt+p#sq6x*ipz2keh741j*-@f@_)f!1t'
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://todo-webapp-production-7324.up.railway.app",
+]
 
 
 # Application definition
