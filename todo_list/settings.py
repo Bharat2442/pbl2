e61953323e2981bbfeff6c69c9ad40e7e89cdbf0
Danu <bharatdanu24@gmail.com>
2025-05-27 01:40:35 +0530

    Initial commit on new GitHub account

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 # ALLOWED HOSTS
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1').split(',')
+ALLOWED_HOSTS = ["*"]
 
 
 # APPLICATIONS
